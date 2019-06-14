f08e1db0d102e3a1d3f16f7595567f9dc91cf21b
Piotr Krupa <piotrkrupa06@gmail.com>
2019-06-14 22:47:47 +0200

    VueJS: Updated to new Api's rule

--- Frontend/vuejs/src/store/EntryStore.js
+++ Frontend/vuejs/src/store/EntryStore.js
@@ -1,4 +1,5 @@
 import EntryApi from "@/api/EntryApi";
+import AccountApi from "../api/AccountApi";
 
 const EntryStore = {
   data: {
@@ -12,11 +13,18 @@ const EntryStore = {
       return await EntryApi.removeEntry(id);
     },
     async updateList() {
-      // console.log("Update...");
-
       const data = await EntryApi.getAllEntries();
 
-      EntryStore.data.entries = data.data;
+      let _entries = data.data;
+
+      for (let i = 0; i < _entries.length; i++)
+      {
+        var author = await AccountApi.getUser(_entries[i].authorId);
+
+        _entries[i].author = author.data;
+      }
+
+      EntryStore.data.entries = _entries;
     }
   }
 };
