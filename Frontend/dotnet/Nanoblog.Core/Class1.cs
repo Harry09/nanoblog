88c3a72d90bc9b2cb24a6960b34838cd015bb5d4
Piotr Krupa <piotrkrupa06@gmail.com>
2019-09-09 23:41:49 +0200

    wpf: added autofac dependency

--- Frontend/dotnet/Nanoblog.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Nanoblog.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ Frontend/dotnet/Nanoblog.Core/Consts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Nanoblog.Core
+{
+    public class Consts
+    {
+        public static readonly string ServerIp = "192.168.0.161";
+        public static readonly ushort ServerPort = 53188;
+    }
+}
