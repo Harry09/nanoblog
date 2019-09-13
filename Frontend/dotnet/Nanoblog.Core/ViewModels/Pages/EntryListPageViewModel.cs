9fbda1b4e7d8a8855b4bea50d75a9e348fc1b9f5
Piotr Krupa <piotrkrupa06@gmail.com>
2019-09-13 02:37:34 +0200

    WPF: Added EntryLIstControl to EntryLIstPage

--- Frontend/dotnet/Nanoblog.Core/ViewModels/Pages/EntryListPageViewModel.cs
+++ Frontend/dotnet/Nanoblog.Core/ViewModels/Pages/EntryListPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Nanoblog.Core.ViewModels.Controls.EntryList;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,50 @@ namespace Nanoblog.Core.ViewModels.Pages
 {
     public class EntryListPageViewModel : BaseViewModel
     {
+        //private EntryListViewModel entryListVM;
+
+        //public EntryListViewModel EntryListViewModel
+        //{
+        //    get => entryListVM;
+        //    set => Update(ref entryListVM, value);
+        //}
+
+        public EntryListViewModel EntryListVM { get; set; } = new EntryListViewModel();
 
         public EntryListPageViewModel()
         {
+            LoadList();
+        }
+
+        public void LoadList()
+        {
+            EntryListVM.List = new ObservableCollection<EntryListItemViewModel>
+            {
+                new EntryListItemViewModel
+                {
+                    UserName = "Obi",
+                    Date = "Today",
+                    Text = "Hello there"
+                },
+                new EntryListItemViewModel
+                {
+                    UserName = "User",
+                    Date = "12 hours ago",
+                    Text = "What a nice application"
+                },
+                new EntryListItemViewModel
+                {
+                    UserName = "Obi",
+                    Date = "Today",
+                    Text = "Hello there"
+                },
+                new EntryListItemViewModel
+                {
+                    UserName = "User",
+                    Date = "12 hours ago",
+                    Text = "What a nice application"
+                }
+            };
         }
     }
 }
