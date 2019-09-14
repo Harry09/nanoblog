ca4fb6ab7beb7ca809e7b761caa3e19ed6040207
Piotr Krupa <piotrkrupa06@gmail.com>
2019-09-15 00:19:11 +0200

    WPF: Added comments

---
+++ Frontend/dotnet/Nanoblog.Core/ViewModels/Controls/CommentList/CommentListItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanoblog.Core.ViewModels.Controls.CommentList
+{
+    public class CommentListItemViewModel : BaseViewModel
+    {
+        private string _userName;
+        private string _date;
+        private string _text;
+
+        public string UserName
+        {
+            get => _userName;
+            set => Update(ref _userName, value);
+        }
+
+        public string Date
+        {
+            get => _date;
+            set => Update(ref _date, value);
+        }
+
+        public string Text
+        {
+            get => _text;
+            set => Update(ref _text, value);
+        }
+    }
+}
---
+++ Frontend/dotnet/Nanoblog.Core/ViewModels/Controls/CommentList/CommentListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Nanoblog.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanoblog.Core.ViewModels.Controls.CommentList
+{
+    public class CommentListViewModel : BaseViewModel
+    {
+
+        private ObservableCollection<CommentListItemViewModel> _list;
+
+        public ObservableCollection<CommentListItemViewModel> List
+        {
+            get => _list;
+            set => Update(ref _list, value);
+        }
+
+        public CommentListViewModel()
+        {
+
+        }
+
+        public void LoadData(IEnumerable<CommentListItemViewModel> data)
+        {
+            List = data.ToObservable();
+        }
+    }
+}
